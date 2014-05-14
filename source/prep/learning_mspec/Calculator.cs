e02ce6c6c2b01a4d7519ffade679b7ab329470ed
Menyueru <maddog28@gmail.com>
2014-05-14 17:43:20 -0400

    Pushing new changes

--- source/prep/learning_mspec/Calculator.cs
+++ source/prep/learning_mspec/Calculator.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.Data;
 namespace prep.learning_mspec
 {
   public class Calculator
   {
+    IDbConnection connection;
+    public Calculator(IDbConnection connection)
+    {
+        this.connection = connection;
+    }
     public int add(int first, int second)
     {
         if(first <0 || second <0) throw new ArgumentException();
+        this.connection.Open();
 	    return first + second;
     }
   }
